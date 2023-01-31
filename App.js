eb91f33d3b72686156757b421526c85a4b49ddf6
xebec19 <rohant946@gmail.com>
2023-02-01 00:04:54 +0530

    chore: app layout solution

--- App.js
+++ App.js
@@ -1,34 +1,20 @@
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
+const isAndroid = Platform.OS === 'android';
+
 export default function App() {
   return (
-    <SafeAreaView
-      style={[
-        styles.container,
-        {
-          flexDirection: "column",
-        },
-      ]}
-    >
-      <View
-        style={{
-          flex: 0.5,
-          backgroundColor: "green",
-          justifyContent: "center",
-        }}
-      >
-        <Text>Search</Text>
-      </View>
-      <View style={{ flex: 5.5, backgroundColor: "blue" }}>
-        <Text>List</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "dodgerblue" }} >
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-});
+const styles = StyleSheet.create({});
